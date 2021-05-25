5d0fd92bb9294420111ea7482e3dee44dfddca54
Jacob Wahlgren <jacobwah@kth.se>
2021-05-25 15:53:04 +0200

    Working!

--- heat.c
+++ heat.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define T_INITIAL 99.0
+#define T_INITIAL 0.0
 #define T_BOUNDARY 100.0
 
 void xerror(const char *s)
@@ -23,8 +23,10 @@ int main(int argc, char **argv)
 	int provided;
 	MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
 
-	int X = 4, Y = 2;
-	int T = 10;
+	assert(argc == 4);
+
+	int X = atoi(argv[1]), Y = atoi(argv[2]);
+	int T = atoi(argv[3]);
 
 	int nprocs;
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -62,8 +64,6 @@ int main(int argc, char **argv)
 	int rank_left, rank_right;
 	MPI_Cart_shift(comm_cart, 1, 1, &rank_left, &rank_right);
 
-	printf("%d: down %d, up %d, left %d, right %d\n", rank, rank_down, rank_up, rank_left, rank_right);
-
 	double (*u0)[n+2] = malloc(sizeof(double)*(n+2)*(n+2));
 	double (*u)[n+2] = malloc(sizeof(double)*(n+2)*(n+2));
 
@@ -72,28 +72,27 @@ int main(int argc, char **argv)
 
 	errno = 0;
 
+	if (rank == 0) {
+			int metafd = open("heat-meta.bin", O_WRONLY|O_CREAT|O_TRUNC, 0600);
+			xerror("meta open");
+			int meta[] = {X, Y, T, nprocs};
+			write(metafd, meta, sizeof(meta));
+			close(metafd);
+	}
+
 	char fname[32] = {};
 	sprintf(fname, "heat-%d-%d.bin", coords[0], coords[1]);
-	int fd = open(fname, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+	int tilefd = open(fname, O_WRONLY|O_CREAT|O_TRUNC, 0600);
 
-	xerror("open");
+	xerror("tile open");
 
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
 			u0[i][j] = T_INITIAL;
 
-	if (rank_up < 0)
-		for (int i = 1; i <= n; i++)
-			u0[i][0] = T_BOUNDARY;
-	if (rank_down < 0)
-		for (int i = 1; i <= n; i++)
-			u0[i][n+1] = T_BOUNDARY;
-	if (rank_left < 0)
-		for (int j = 1; j <= n; j++)
-			u0[0][j] = T_BOUNDARY;
-	if (rank_right < 0)
-		for (int j = 1; j <= n; j++)
-			u0[n+1][j] = T_BOUNDARY;
+	// If there is no sending neighbor, there must be a boundary
+	for (int i = 0; i < n*4; i++)
+		recvbuf[i] = T_BOUNDARY;
 
 	for (int t = 0; t < T; t++) {
 		// Order: down, up, left, right
@@ -102,9 +101,9 @@ int main(int argc, char **argv)
 		for (int i = 1; i <= n; i++)
 			sendbuf[1*n + i-1] = u0[n][i];
 		for (int j = 1; j <= n; j++)
-			sendbuf[2*n + j-1] = u0[1][n];
+			sendbuf[2*n + j-1] = u0[j][1];
 		for (int j = 1; j <= n; j++)
-			sendbuf[3*n + j-1] = u0[n][1];
+			sendbuf[3*n + j-1] = u0[j][n];
 
 		MPI_Neighbor_alltoall(sendbuf, n, MPI_DOUBLE, recvbuf, n, MPI_DOUBLE, comm_cart);
 
@@ -126,8 +125,8 @@ int main(int argc, char **argv)
 		}
 
 		for (int j = 1; j <= n; j++) {
-			write(fd, &u[j][1], sizeof(double)*n); 
-			xerror("write");
+			write(tilefd, &u[j][1], sizeof(double)*n);
+			xerror("tile write");
 		}
 
 		void *tmp = u0;
