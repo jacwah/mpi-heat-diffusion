3e650b259d04ac14cf30d1aeec3128925f1d4c0d
Jacob Wahlgren <jacob@dstsrc.net>
2021-06-07 17:32:01 +0200

    Imporve visualizations

--- idle.py
+++ idle.py
@@ -31,7 +31,7 @@ for i in range(xn):
                 if size >= 1.34857622e+08:
                     lines.append(((i, j, ints[2*k+1]), (i, j, ints[2*k+0])))
                 # lines2.append(((j+i/xn, ints[2*k+1]), (j+i/xn, ints[2*k+0])))
-                # color2.append(colors.hsv_to_rgb((i/xn, 0.5, 1.0)))
+                # color2.append(colors.hsv_to_rgb((i/xn, 1.0, 1.0)))
 
 lens = np.array(lens)
 percs = np.array([90, 99, 99.9])
@@ -43,6 +43,10 @@ fig = plt.figure()
 # linecoll = collections.LineCollection(lines2, color=color2)
 # ax.set_xlim(0, yn-1)
 # ax.set_ylim(0, np.max(lines))
+# ax.set_xlabel("process y")
+# ax.set_ylabel("cycles")
+# ax.set_title("Idle periods")
+# ax.set_xticks(list(range(16)))
 
 ax = fig.add_subplot(projection='3d', title="99 percentile idle periods")
 ax.set_xlim(0, xn-1)
--- perfplot.py
+++ perfplot.py
@@ -5,10 +5,17 @@ t = np.array([float(x) for x in open("heat-time", "r").readlines()])
 
 n = np.array([1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 196, 225, 256])
 
-plt.xlabel("Number of processes")
-plt.ylabel("Execution time (s)")
-plt.loglog(n, t, marker="o")
-plt.title("Strong scaling")
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
-plt.savefig("perfplot.png")
+ax1.set_title("Log-log execution time")
+ax1.set_xlabel("Number of processes")
+ax1.set_ylabel("Execution time (s)")
+ax1.loglog(n, t, marker="o")
+
+ax2.set_title("Speedup")
+ax2.set_xlabel("Number of processes")
+ax2.set_ylabel("Speedup")
+ax2.plot(n, t[0]/t, marker="o")
+ax2.plot([0, 100], [0, 100], ls="--")
+
 plt.show()
